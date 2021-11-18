f9134c3a565b52ed2e0dcaf83a522b6022609703
Zychkov <48855208+Zychkov@users.noreply.github.com>
2021-11-18 21:35:27 +0300

    init

---
+++ src/main/java/com/example/demo/entity/ImageModel.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Data
+@Entity
+public class ImageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageBytes;
+
+    @JsonIgnore
+    private Long userId;
+
+    @JsonIgnore
+    private Long postId;
+
+    public ImageModel() {
+    }
+}
